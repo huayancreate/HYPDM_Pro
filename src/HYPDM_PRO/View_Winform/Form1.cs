681c17871cdf35f7bfb269d72b8972c1664d8352
hywxu <wxu@huayancreate.com>
2013-12-10 15:28:03 +0800

    View工程创建

---
+++ src/HYPDM_PRO/View_Winform/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Helpers;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+using DevExpress.UserSkins;
+
+
+namespace View_Winform
+{
+    public partial class Form1 : RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitSkinGallery();
+            InitGrid();
+
+        }
+        void InitSkinGallery()
+        {
+            SkinHelper.InitSkinGallery(rgbiSkins, true);
+        }
+        BindingList<Person> gridDataList = new BindingList<Person>();
+        void InitGrid()
+        {
+            gridDataList.Add(new Person("John", "Smith"));
+            gridDataList.Add(new Person("Gabriel", "Smith"));
+            gridDataList.Add(new Person("Ashley", "Smith", "some comment"));
+            gridDataList.Add(new Person("Adrian", "Smith", "some comment"));
+            gridDataList.Add(new Person("Gabriella", "Smith", "some comment"));
+            gridControl.DataSource = gridDataList;
+        }
+
+    }
+}
---
+++ src/HYPDM_PRO/View_Winform/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("View_Winform")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("View_Winform")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b6968e8-be60-45f8-ab0f-9f4c10653136")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
