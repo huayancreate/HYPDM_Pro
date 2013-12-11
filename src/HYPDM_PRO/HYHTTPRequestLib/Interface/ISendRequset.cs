1f1c8473160ae19da522b524936799a848df65a0
hywxu <wxu@huayancreate.com>
2013-12-11 14:29:04 +0800

    xuwei

---
+++ src/HYPDM_PRO/HYHTTPRequestLib/Interface/ISendRequset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HYHTTPRequestLib.Interface
+{
+    interface ISendRequset
+    {
+        bool AsynSend(string url, Dictionary<string, string> data);
+        bool SynSend(string url, Dictionary<string, string> data);
+    }
+}
