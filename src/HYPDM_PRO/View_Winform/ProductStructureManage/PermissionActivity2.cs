05126518f6a1902cf30c66d07a9854018321c7d5
hycjxu <cjxu@huayancreate.com>
2013-12-19 14:43:58 +0800

    界面原型修改列表

---
+++ src/HYPDM_PRO/View_Winform/ProductStructureManage/PermissionActivity2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace View_Winform.ProductStructureManage
+{
+    public partial class PermissionActivity2 : DevExpress.XtraEditors.XtraForm
+    {
+        public PermissionActivity2()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- src/HYPDM_PRO/View_Winform/ProductStructureManage/ReadMe.txt
+++ src/HYPDM_PRO/View_Winform/ProductStructureManage/ReadMe.txt
@@ -2,3 +2,8 @@
      产品结构管理   ProductStructureManage
 3.1
 	 BOM信息维护    BOMInformationMaintion
+
+	 查看BOM信息    CheckBOMMessage
+
+	 权限设置       PermissionActivity
+
