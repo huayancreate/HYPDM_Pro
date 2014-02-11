16fa31230bd3dba41ad993e3a179da7b2e96cdf6
hywxu <wxu@huayancreate.com>
2014-02-11 11:23:36 +0800

    xuwei

---
+++ src/HYPDM_PRO/WcfConfigCenterConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WcfExtension.ConfigCenter;
+using WcfExtension;
+using System.Threading;
+using System.ServiceModel;
+using System.Configuration;
+
+namespace WcfConfigCenterConsole
+{
+    /// <summary>
+    /// This is the console host of config service, just for test.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigUpdateManager.InitHeart();
+
+            try
+            {
+                using (var host = WcfServiceHostFactory.CreateServiceHost<WcfConfigService>())
+                {
+                    host.Open();
+                    Console.WriteLine("Config service started");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ src/HYPDM_PRO/WcfLogCenterConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WcfExtension;
+using WcfExtension.LogCenter;
+using WcfExtension.LogCenter.Core;
+
+namespace WcfLogCenterConsole
+{
+    /// <summary>
+    /// This is the console host of log service, just for test.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WcfLogServer.Start();
+            try
+            {
+                using (var host = WcfServiceHostFactory.CreateServiceHost<WcfLogService>())
+                {
+                    host.Open();
+                    Console.WriteLine("Log service started");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+
+            WcfLogServer.End();
+        }
+    }
+}
+
